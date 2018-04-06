02ca8457c3c433d0ba0a24978dcb213e3b8391ee
xmfaly <xmfaly@gmail.com>
2018-04-06 19:04:26 +0800

    修复脚本错误

---
+++ src/main/java/cc/lylllcc/service/ClassRoomService.java
@@ -0,0 +1,12 @@
+package cc.lylllcc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassRoomService {
+
+
+    public Object getClassRoom(String username){
+        return 1;
+    }
+}
